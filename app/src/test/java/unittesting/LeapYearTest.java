1d23d05b45f5baa51f3ae95b7d419f7caff83ed5
alesearm <alesearm@students.zhaw.ch>
2024-05-10 01:02:37 +0200

    Testfälle kreiert

--- app/src/test/java/unittesting/LeapYearTest.java
+++ app/src/test/java/unittesting/LeapYearTest.java
@@ -1,10 +1,14 @@
 package unittesting;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
     @Test
     public void testIsLeapYear() {
-
+        assertFalse(LeapYear.isLeapYear(2023)); // 2023 ist nicht durch 4 teilbar
+        assertTrue(LeapYear.isLeapYear(2024));  // 2024 ist durch 4 teilbar
     }
 }
 
