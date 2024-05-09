ea6ca539ab8b9979a02755c1f0f3ab75324604d5
alesearm <alesearm@students.zhaw.ch>
2024-05-10 01:18:37 +0200

    Testfälle erweitert (korrektur Syntax)

--- app/src/main/java/unittesting/LeapYear.java
+++ app/src/main/java/unittesting/LeapYear.java
@@ -3,8 +3,15 @@ package unittesting;
 public class LeapYear {
 
     public static boolean isLeapYear(int year) {
-        return year % 4 == 0;
-    }
-}
+        if (year % 400 == 0) {
+            return true; 
+        } else if (year % 100 == 0) {
+            return false; 
+        } else if (year % 4 == 0) {
+            return true; 
+        } else {
+            return false; 
+        }
+    } 
 
 
