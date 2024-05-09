3c5997acce7ed0e8129c8a24d7a8c92c5b9712a6
alesearm <alesearm@students.zhaw.ch>
2024-05-10 01:31:56 +0200

    Code Refactoring

--- app/src/main/java/unittesting/LeapYear.java
+++ app/src/main/java/unittesting/LeapYear.java
@@ -3,16 +3,6 @@ package unittesting;
 public class LeapYear {
 
     public static boolean isLeapYear(int year) {
-        if (year % 400 == 0) {
-            return true; 
-        } else if (year % 100 == 0) {
-            return false; 
-        } else if (year % 4 == 0) {
-            return true; 
-        } else {
-            return false; 
-        }
-    } 
-} 
-
-
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+}
